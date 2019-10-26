8058f5128c41bcaa24416dc22949ec395068166b
Przemysław Turalski <34220668+turlejj@users.noreply.github.com>
2019-10-26 12:21:47 +0200

    new file to create new branch
    Missing master branch issue

---
+++ helloWorld.java
@@ -0,0 +1,11 @@
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		String hello = "Hello world!";
+		
+		System.out.println("Napis 'Hello world' ma " + hello.length() + "znakow.");
+	}
+}
