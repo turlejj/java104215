f0b7019db548b344dfaddb4f2d6f60641abce1a5
Przemysław Turalski <34220668+turlejj@users.noreply.github.com>
2019-11-08 22:03:20 +0100

    Quiz

---
+++ Quiz.java
@@ -0,0 +1,14 @@
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Quiz {
+
+	String Question;
+	String[] AnswerOptions = new String[4];
+	String Answer;
+}
