ab558a261b917f57126dd64f1cd886e37ba6becf
Przemysław Turalski <34220668+turlejj@users.noreply.github.com>
2019-11-16 12:52:02 +0100

    lowerCamelCase

--- Car/ZadanieCar.java
+++ Car/ZadanieCar.java
@@ -5,9 +5,9 @@ public class ZadanieCar {
 	public static void main(String[] args) {
 		Mercedes MercedesCar = new Mercedes();
 		
-		MercedesCar.SetBrand("Mercedes");
+		MercedesCar.setBrand("Mercedes");
 		
-		System.out.println("Im a car! And Im a " + MercedesCar.GetBrand());
+		System.out.println("Im a car! And Im a " + MercedesCar.getBrand());
 	}
 
 }
