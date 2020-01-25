7856e8f2a684ca141c475af70129c59360ec3190
Przemysław Turalski <34220668+turlejj@users.noreply.github.com>
2020-01-25 11:05:59 +0100

    Zadanie 3

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 
 public class Main {
-
+	
 	public static void main(String[] args) throws InterruptedException {
 		
-		for(int i = 1; i < 20;) {
-			JoinPractice obj1 = new JoinPractice(i,i+1);
-			obj1.start();
-			obj1.join();
-			
-			i+=2;
-		}
+		Account account = new Account(100);
+		account.start();
+		account.join();
 
+		Account account1 = new Account(200);
+		account1.start();
+		account.join();
 	}
+
+
 }
